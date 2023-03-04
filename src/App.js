d4a1e547ac883c19869b81ce93743aaf38523986
bl1nkker <nurovich14@gmail.com>
2023-03-04 22:36:44 +0600

    feat: added anime page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Navbar from "./common/components/Navbar";
 import Cart from "./features/shop/views/Cart";
 import Home from "./features/shop/views/Home";
 import Todo from "./features/todo/views/Todo";
+import Anime from "./features/anime/views/Anime";
 function App() {
   const view = useSelector((state) => state.view);
   return (
     <div>
       <Navbar />
-      {view === "cart" ? <Cart /> : view === "todo" ? <Todo /> : <Home />}
+      {view === "cart" ? (
+        <Cart />
+      ) : view === "todo" ? (
+        <Todo />
+      ) : view === "anime" ? (
+        <Anime />
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
---
+++ src/features/anime/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <h1>
+        Anime<strong>Anime</strong>Database
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
